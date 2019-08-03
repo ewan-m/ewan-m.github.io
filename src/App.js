9f52ab72986e89c55875fc7dd5a60d6e66e76623
Ewan Morrison <ewanmorrison@hotmail.co.uk>
2019-08-03 17:46:31 +0100

    create main app container with router outlet and layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SideMenu from './Pages/SideMenu';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Search from './Pages/Search';
+import About from './Pages/About';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App d-flex flex-row">
+                <SideMenu />
+                <div className="PageContainer w-100 p-3">
+                    <Route path="/search" component={Search} />
+                    <Route path="/about" component={About} />
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
